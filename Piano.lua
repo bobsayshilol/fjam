83efa87a90012b938591a2453f6827817f8268d4
bobsayshilol <bobsayshilol@live.co.uk>
2021-04-03 12:51:05 +0100

    Add springs that show their amplitude.

--- Piano.lua
+++ Piano.lua
@@ -5,8 +5,13 @@ local Key = assert(require("Key"))
 local keyHeight = 0.08
 local leftHeight = 0.42
 
+local function backPosition(x)
+	local y0 = keyHeight + leftHeight
+	return y0 / (1 - x * (1 - y0))
+end
+
 local function makeKeys(x, y, width, height, octaves)
-	-- TODO: the w/b keys are all wrong
+	-- TODO: the w/b keys are wrong sizes
 	local numKeys = octaves * 12
 	local w = width / numKeys
 	local h = height * keyHeight
@@ -19,15 +24,27 @@ local function makeKeys(x, y, width, height, octaves)
 	return keys
 end
 
+local function makeSprings(x, y, width, height, octaves)
+	local numKeys = octaves * 12
+	local y0 = y + keyHeight * height
+	local springs = {}
+	for i = 1,numKeys do
+		local rx = (i - 0.5) / numKeys
+		local px = x + rx * width
+		local py = y + backPosition(rx) * height
+		table.insert(springs, Spring(px, y0, px, py))
+	end
+	return springs
+end
+
 local function makeBorder(x, y, width, height)
 	local points = { 1,0, 0,0, }
-	local y0 = keyHeight + leftHeight
 	local steps = 10
 	
 	local dx = 1/steps
 	for i = 0,steps do
 		local x = dx * i
-		local y = y0 / (1 - x * (1 - y0))
+		local y = backPosition(x)
 		table.insert(points, x)
 		table.insert(points, y)
 	end
@@ -43,9 +60,10 @@ end
 
 function ctor(x, y, width, height)
 	local Piano = {}
-	Piano.springs = {}
+	local octaves = 3
 	Piano.border = makeBorder(x, y, width, height)
-	Piano.keys = makeKeys(x, y, width, height, 3)
+	Piano.keys = makeKeys(x, y, width, height, octaves)
+	Piano.springs = makeSprings(x, y, width, height, octaves)
 
 	Piano.update = function(self, dt)
 		for u,spring in pairs(self.springs) do
@@ -54,19 +72,19 @@ function ctor(x, y, width, height)
 	end
 	
 	Piano.draw = function(self)
-		-- Full border
-		love.graphics.setColor(1, 1, 1)
-		love.graphics.polygon("line", self.border)
+		-- Springs
+		for u,spring in pairs(self.springs) do
+			spring:draw()
+		end
 		
 		-- Keys
 		for u,key in pairs(self.keys) do
 			key:draw()
 		end
 		
-		-- Springs
-		for u,spring in pairs(self.springs) do
-			spring:draw()
-		end
+		-- Full border
+		love.graphics.setColor(1, 1, 1)
+		love.graphics.polygon("line", self.border)
 	end
 
 	return Piano
--- Spring.lua
+++ Spring.lua
@@ -1,14 +1,29 @@
 
+local sound
 
-function ctor()
+local fadeFactor = 0.4
+
+function ctor(x0,y0, x1,y1)
 	local Spring = {}
-
+	Spring.x0 = x0
+	Spring.y0 = y0
+	Spring.x1 = x1
+	Spring.y1 = y1
+	Spring.amplitude = 1
+	
 	Spring.update = function(self, dt)
+		self.amplitude = self.amplitude * (1 - fadeFactor * dt)
 	end
 	
 	Spring.draw = function(self)
+		local amp = self.amplitude
+		love.graphics.setColor(amp, amp, 0)
+		local oldWidth = love.graphics.getLineWidth()
+		love.graphics.setLineWidth(6)
+		love.graphics.line(self.x0,self.y0, self.x1,self.y1)
+		love.graphics.setLineWidth(oldWidth)
 	end
-
+	
 	return Spring
 end
 
