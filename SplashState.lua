55c43358c2e23ea91aa631283635da6283afe894
bobsayshilol <bobsayshilol@live.co.uk>
2021-04-03 08:41:36 +0100

    Start with space rather than any button

--- SplashState.lua
+++ SplashState.lua
@@ -6,21 +6,20 @@ end
 
 function class.new()
 	local state = {}
-	state.t = 0
 	state.next = nil
 
 	state.update = function(self, dt)
-		self.t = self.t + dt
 		return state.next
 	end
 	
 	state.keypressed = function(self, key)
-		print("Pressed at " .. self.t)
-		state.next = "intro"
+		if key == "space" then
+			state.next = "intro"
+		end
 	end
 	
 	state.draw = function(self)
-		love.graphics.print("Press any button to start", 10, 10)
+		love.graphics.print("Press space to start", 10, 10)
 	end
 
 	return state
