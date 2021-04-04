1a8a8d9a7c939c9e6505e20a00865bfe064b5a4a
bobsayshilol <bobsayshilol@live.co.uk>
2021-04-04 19:18:43 +0100

    Use the spring's co-ords to stop it from being bounds checked

--- Piano.lua
+++ Piano.lua
@@ -168,7 +168,9 @@ function ctor(x, y, width, height, broken)
 		-- Calculate how close this was
 		local sx,sy = spring.x0, spring.y0
 		local baseL = spring.baseL
-		local x,y = love.mouse.getPosition()
+		-- Don't use the mouse as it's not bounds checked
+		--local x,y = love.mouse.getPosition()
+		local x,y = spring.x1, spring.y1
 		local length = math.sqrt((x - sx) ^ 2 + (y - sy) ^ 2)
 		local distance = math.abs(length - baseL)
 		local closeness = 1 - math.min(math.max(distance - hitTolerance/2, 0) / hitTolerance, 1)
